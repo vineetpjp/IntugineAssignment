0fc611d4bf25d78dfc57887eb36a6e3e077159b2
vineet <vineetpjp@gmail.com>
2020-11-21 17:21:56 +0530

    counters completed

---
+++ src/components/Info/InfoBox.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const InfoBox = () => {
+  return (
+    <div className="info--box info--box__active">
+      <div>
+        <h4 className="info--box__text1">DEL</h4>
+      </div>
+      <div className="info--box__text2">
+        <p>916</p>
+      </div>
+    </div>
+  );
+};
+
+export default InfoBox;
---
+++ src/components/Info/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import InfoBox from "./InfoBox";
+import "./index.scss";
+
+const Info = () => {
+  return (
+    <div className="info">
+      <InfoBox />
+      <InfoBox />
+      <InfoBox />
+      <InfoBox />
+      <InfoBox />
+    </div>
+  );
+};
+
+export default Info;
