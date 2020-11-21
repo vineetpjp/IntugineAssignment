e100bc43e0eb4ab14eacd0397829506f156984bb
vineet <vineetpjp@gmail.com>
2020-11-21 17:23:15 +0530

    Helper components for table and list outline

---
+++ src/components/utils/ContainerOutline/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./index.scss";
+
+const ContainerOutline = (props) => {
+  return <div className="container-outline">{props.children}</div>;
+};
+
+export default ContainerOutline;
---
+++ src/components/utils/index.js
@@ -0,0 +1,4 @@
+import ContainerOutline from "components/utils/ContainerOutline";
+import ListContainer from "components/utils/ListContainer";
+
+export { ContainerOutline, ListContainer };
